92c62fba8c086a001f55f2011282dd70c9560e1d
Vu Minh Tri <vmtniichov@gmail.com>
2022-04-23 16:48:11 +0700

    Alow cors for FE

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -8,11 +8,17 @@ app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
 
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        # allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
