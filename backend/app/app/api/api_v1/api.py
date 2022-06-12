510b9a576f65e7fec547e42182a614a90c5e59e8
Tri Vu <tri.vu@zien.vn>
2022-06-12 10:22:25 +0700

    Update router for user word

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -4,7 +4,7 @@ from .endpoints import words, users, login, subject, user_words
 
 api_router = APIRouter()
 api_router.include_router(words.router, prefix="/words", tags=["words"])
-api_router.include_router(words.router, prefix="/user_words", tags=["user_words"])
+api_router.include_router(user_words.router, prefix="/user_words", tags=["user_words"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(subject.router, prefix="/subject", tags=["subject"])
 api_router.include_router(login.router, tags=["login"])
